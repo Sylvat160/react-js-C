1650bd227ae3e0b207cd8b0a7e56939db2564d4a
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-10-31 17:55:44 +0000

    mise en place des page racine et 404

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Home from './pages/Home';
 import Contact from '@/pages/Contact';
 import Service from '@/pages/Service';
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import Error from '@/_utils/Error';
 
 function App() {
   return (
     <div className="App">
-          <Home/>
-          <Contact/>
-          <Service/>
+      <BrowserRouter> 
+        <Routes>
+          <Route index element={<Home/>}/>
+
+
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/service' element={<Service/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='*' element={<Error/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
       
   );
