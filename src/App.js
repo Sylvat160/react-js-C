ad14d7506a8172dbdfa9271173b3a973313db083
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-10-31 17:35:33 +0000

    craco configure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Service from './pages/Service';
+import Contact from '@/pages/Contact';
+import Service from '@/pages/Service';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
           <Home/>
           <Contact/>
           <Service/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
+      
   );
 }
 
