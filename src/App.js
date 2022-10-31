3e726c38162c02d8abe49a03445cc8ac31b5ba91
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-10-31 18:31:45 +0000

    Layout termine

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 // import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-import Contact from '@/pages/Contact';
-import Service from '@/pages/Service';
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
-import Error from '@/_utils/Error';
+import "./App.css";
+import Home from "./pages/Home";
+import Contact from "@/pages/Contact";
+import Service from "@/pages/Service";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error from "@/_utils/Error";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter> 
+      <BrowserRouter>
         <Routes>
-          <Route index element={<Home/>}/>
+          <Route element={<Layout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/service" element={<Service />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
 
+          <Route index element={<Home />} />
 
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/service' element={<Service/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='*' element={<Error/>}/>
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </div>
-      
   );
 }
 
