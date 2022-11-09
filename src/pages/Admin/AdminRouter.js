b30951a9c49b31a2cfdd7b01708aed8c49042dae
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-11-09 10:51:20 +0000

    User route

--- src/pages/Admin/AdminRouter.js
+++ src/pages/Admin/AdminRouter.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
  
 import {ALayout , Dashboard} from '@/pages/Admin';
+import { User , UserAdd , UserEdit } from '@/pages/Admin/User';
+import { Cocktail , CocktailEdit } from '@/pages/Admin/Cocktail';
 import Error from "@/_utils/Error";
 
 
