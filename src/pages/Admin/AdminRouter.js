ca8b6382121f0861315e4279d9de7049b5e48b84
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-11-09 11:33:30 +0000

    Cocktail route

--- src/pages/Admin/AdminRouter.js
+++ src/pages/Admin/AdminRouter.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
- 
-import {ALayout , Dashboard} from '@/pages/Admin';
-import { User , UserAdd , UserEdit } from '@/pages/Admin/User';
-import { Cocktail , CocktailEdit } from '@/pages/Admin/Cocktail';
+
+import { ALayout, Dashboard } from "@/pages/Admin";
+import { User, UserAdd, UserEdit } from "@/pages/Admin/User";
+import { Cocktail, CocktailEdit } from "@/pages/Admin/Cocktail";
 import Error from "@/_utils/Error";
 
-
 const AdminRouter = () => {
-    return (
-      <Routes>
-        <Route element={<ALayout />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="*" element={<Error />} />
+  return (
+    <Routes>
+      <Route element={<ALayout />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="user">
+          <Route index element={<User />} />
+          <Route path="add" element={<UserAdd />} />
+          <Route path="edit" element={<UserEdit />} />
         </Route>
-      </Routes>
-    );
+        <Route path="cocktail">
+          <Route index element={<Cocktail />} />
+          <Route path="add" element={<CocktailEdit />} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default AdminRouter;
