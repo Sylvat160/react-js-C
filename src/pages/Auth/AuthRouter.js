5730f03b6b1bfeaca60bc3fd8bbac315b8a444ca
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-11-10 15:42:33 +0000

    login route set up

--- src/pages/Auth/AuthRouter.js
+++ src/pages/Auth/AuthRouter.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes , Route} from 'react-router-dom';
 
 import Login from '@pages/Auth/Login';
+import Error from '@pages/_utils/Error';
 
 const AuthRouter = () => {
     return (
         <Routes>
             <Route path="login" element={<Login />} />
+            <Route path="*" element={<Error />} />
         </Routes>
     );
 };
