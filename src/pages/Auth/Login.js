da6da2e130fd5d297ecb7d929a63faf59b251089
sylvain tagnabou <sylvaintagnabou@gmail.com>
2022-11-10 17:08:29 +0000

    form with state

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -3,20 +3,21 @@ import { useState } from 'react';
 
 const Login = () => {
     const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log('submit');
+        console.log(login , password);
     }
     return (
         <div className="login-box">
-        <h2>Login</h2>
+        <h2>Login  </h2>
     <form onSubmit={onSubmit}>
         <div className="user-box group">
-            <input type="text" name="login"  required/>
+            <input type="text" name="login" value={login} onChange={e=>setLogin(e.target.value)}  required/>
             <label htmlFor='login'>Username</label>
         </div>
         <div className="user-box group">
-            <input type="password" name="password" required=""/>
+            <input type="password" name="password" value={password} onChange={e=>setPassword(e.target.value)} required=""/>
             <label htmlFor='password'>Password</label>
         </div>
         <a href="#">
