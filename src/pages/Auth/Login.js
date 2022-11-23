bc83c4bd09ea3c263d15324dc9afc51175b4d7cf
Sylvain Tagnabou <sylvaintagnabou@gmail.com>
2022-11-23 19:39:09 +0000

    login form refactoring

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -2,22 +2,34 @@ import React from 'react';
 import { useState } from 'react';
 
 const Login = () => {
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
+    // const [login, setLogin] = useState('');
+    // const [password, setPassword] = useState('');
+
+    const onChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const [credentials, setCredentials] = useState({
+        login : '',
+        password : ''
+    });
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log(login , password);
+        console.log(credentials);
     }
     return (
         <div className="login-box">
         <h2>Login  </h2>
     <form onSubmit={onSubmit}>
         <div className="user-box group">
-            <input type="text" name="login" value={login} onChange={e=>setLogin(e.target.value)}  required/>
+            <input type="text" name="login" value={credentials.login} onChange={onChange} required/>
             <label htmlFor='login'>Username</label>
         </div>
         <div className="user-box group">
-            <input type="password" name="password" value={password} onChange={e=>setPassword(e.target.value)} required=""/>
+            <input type="password" name="password" value={credentials.password} onChange={onChange} required=""/>
             <label htmlFor='password'>Password</label>
         </div>
         <a href="#">
