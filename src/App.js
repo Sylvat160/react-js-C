9d32d9c609e3f40fd73160043632744b9a1fc6b3
Sylvain T <sylvaintagnabou@gmail.com>
2023-03-19 19:34:18 +0000

    fetchibg ombg api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=1fba06d8'
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+
+        console.log(data)
+    }
+
+    useEffect(() => {
+        searchMovies('Iron man')
+    }, [])
     return (
         <>
         <h1>
